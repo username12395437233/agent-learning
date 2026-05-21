734d1247ae2d4e5f85743da4efdc0479912b91e9
Alexandr Zarubin <zarubin_14@list.ru>
2026-05-21 16:21:08 +0300

    chore: add eslint rule

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config(
   {
@@ -35,22 +36,12 @@ export default tseslint.config(
       },
     },
     rules: {
-      // React 17+ / Vite не требует import React
       'react/react-in-jsx-scope': 'off',
-
-      // Часто мешает в TS, потому что типы уже описывают props
       'react/prop-types': 'off',
-
-      // Хорошее style-правило для JSX
-      'react/jsx-boolean-value': ['warn', 'never'],
-
-      // Хорошее style-правило для компонентов
       'react/self-closing-comp': 'warn',
-
-      // Для shadcn/ui и composition patterns иногда удобнее не запрещать spread
+      'react/jsx-boolean-value': ['warn', 'never'],
       'react/jsx-props-no-spreading': 'off',
 
-      // TS-style
       '@typescript-eslint/consistent-type-imports': [
         'warn',
         {
@@ -68,4 +59,6 @@ export default tseslint.config(
       ],
     },
   },
+
+  eslintConfigPrettier,
 );
